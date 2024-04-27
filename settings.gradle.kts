974b4040cd2a61dbd592391037f36a0970954422
amalyarzky <amalyarzky@gmail.com>
2024-04-27 21:14:52 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Studikasus2_13120220012"
+include(":app")
+ 
